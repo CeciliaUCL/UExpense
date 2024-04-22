31ec4d7b77ab93d7fafb5eeb4dd2e3c0b5f70ee4
CeciliaUCL <ucab206@ucl.ac.uk>
2024-04-22 20:24:00 +0100

    Test Version1

--- myfirstapp/lib/Pages/Dashboard.dart
+++ myfirstapp/lib/Pages/Dashboard.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:myfirstapp/Pages/WelcomePage.dart';
 import 'package:myfirstapp/Pages/AddCostPage.dart';
 import 'package:myfirstapp/Pages/ChangePasswordPage.dart';
 import 'package:myfirstapp/tool/DBExTool.dart';
+import 'package:http/http.dart' as http;
 
 import '../entity/RecordDetails.dart';
 
@@ -16,6 +22,11 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
 
   int _selectedIndex = 0;
+
+  List<dynamic> _hourlyWeather = [];//store weather
+  List<String> messages = [];  // store chat
+  TextEditingController messageController = TextEditingController();
+
 
   double expenseTotal = 0;
   double incomeTotal = 0;
@@ -83,7 +94,7 @@ class _DashboardState extends State<Dashboard> {
       //This is the profile page which will have admin and logout options
       _detailPage(),
       // Placeholder for detail page
-      Text('Assistant Page'),
+      _assistantPage(),
       // Placeholder for assistant page
       _profilePage(),
       // This is the profile page which will have admin and logout options
@@ -111,6 +122,7 @@ class _DashboardState extends State<Dashboard> {
         },
         child: Icon(Icons.add, size: 36),
         backgroundColor: Color.fromARGB(255, 51, 126, 111),
+        foregroundColor: Colors.white,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       bottomNavigationBar: BottomAppBar(
@@ -279,20 +291,25 @@ class _DashboardState extends State<Dashboard> {
   }
 
   // Account balance section
-  Widget _accountBalanceSection() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _balanceCard(
+Widget _accountBalanceSection() {
+  return Container(
+    padding: EdgeInsets.all(16),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: _balanceCard(
               'Income', incomeTotal, Icons.arrow_downward, Colors.green),
-          _balanceCard(
-              'Expenses', expenseTotal, Icons.arrow_upward, Colors.red),
-        ],
-      ),
-    );
-  }
+        ),
+        Expanded(
+          child: _balanceCard(
+              'Expenditure', expenseTotal, Icons.arrow_upward, Colors.red),
+        ),
+      ],
+    ),
+  );
+}
+
 
   // Date filter section
   Widget _dateFilterSection() {
@@ -333,13 +350,10 @@ class _DashboardState extends State<Dashboard> {
       ),
     );
   }
-
+//——————————————————————————————————————Profile——————————————————————————————————————————————————————————
   Widget _profilePage() {
-    // User? user = FirebaseAuth.instance.currentUser;
-    // String displayName = user?.email ??
-    //     "No name available"; // Default text if user name is not available
-
-    String displayName = "Test";
+    User? user = FirebaseAuth.instance.currentUser;
+    String displayName = user?.email ?? "No email available"; // Default text if user email is not available
 
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -353,8 +367,7 @@ class _DashboardState extends State<Dashboard> {
           title: Text('Change Password'),
           onTap: () {
             // Navigate to change password page
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => ChangePasswordPage()));
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChangePasswordPage()));
           },
         ),
         ListTile(
@@ -362,7 +375,7 @@ class _DashboardState extends State<Dashboard> {
           title: Text('Logout'),
           onTap: () {
             // Handle logout tap
-            // FirebaseAuth.instance.signOut();
+            FirebaseAuth.instance.signOut();
             Navigator.of(context).pushAndRemoveUntil(
               MaterialPageRoute(builder: (context) => WelcomePage()),
               (Route<dynamic> route) => false,
@@ -372,4 +385,271 @@ class _DashboardState extends State<Dashboard> {
       ],
     );
   }
+  
+
+
+//————————————————————————————————————————————————Assistant————————————————————————————————————————————————————————
+ //send messaege and return reply
+  Future<void> sendMessage() async {
+    final String userInput = messageController.text;
+    if (userInput.isNotEmpty) {
+      setState(() {
+        messages.insert(0, "You: $userInput");
+      });
+      final String response = await chatWithOpenAI(userInput);
+      setState(() {
+        messages.insert(0, "OpenAI: $response");
+      });
+      messageController.clear();
+    }
+  }
+
+  //fecth APIKey
+  Future<String> fetchApiKey() async {
+    try {
+      final response = await http.get(Uri.parse('https://weicheng.app/somesecrettttt.txt'));
+      if (response.statusCode == 200) {
+        return response.body.trim();  // return API from sever
+      } else {
+        throw Exception('Failed to load API key');
+      }
+    } catch (e) {
+      throw Exception('Failed to load API key: $e');
+    }
+  }
+
+  //get OpenAI API
+  Future<String> chatWithOpenAI(String message) async {
+  try {
+    final apiKey = await fetchApiKey();  // get API
+    final url = Uri.parse('https://api.openai.com/v1/chat/completions');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey'
+      },
+      body: jsonEncode({
+        'messages': [{'role': 'user', 'content': message}],
+        'model': 'gpt-3.5-turbo',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      return jsonResponse['choices'][0]['message']['content'].trim();
+    } else {
+      throw Exception('Failed to get response: ${response.body}');
+    }
+  } catch (e) {
+    return 'Error contacting OpenAI: $e';
+  }
+}
+
+  //press the button to show weather and location
+  String _weatherInfo = 'Press the button to get weather';
+  Future<void> getWeather() async {
+    try {
+      Position position = await determinePosition();
+      var currentWeatherData = await fetchWeather(position.latitude, position.longitude);
+      var hourlyWeatherData = await fetchHourlyWeather(position.latitude, position.longitude);
+      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+      String place = placemarks.isNotEmpty ? '${placemarks.first.locality}, ${placemarks.first.country}' : 'Unknown location';
+      setState(() {
+        _weatherInfo = 'Current Temperature: ${_convertToCelsius(currentWeatherData['main']['temp']).toStringAsFixed(1)} °C\n'
+               'Weather: ${currentWeatherData['weather'][0]['description']}\n'
+               'Location: $place';
+        _hourlyWeather = hourlyWeatherData['list'].take(8).toList();//for 24h
+      });
+    } catch (e) {
+      setState(() {
+        _weatherInfo = 'Failed to get weather: $e';//handle failure
+        _hourlyWeather = [];
+      });
+    }
+  }
+  
+  //convert to celsius
+  double _convertToCelsius(double tempInKelvin) {
+    return tempInKelvin - 273.15;
+  }
+  
+  //record location
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  //use openweathermap API to get current weather
+  Future<Map<String, dynamic>> fetchWeather(double latitude, double longitude) async {
+    final apiKey = '69516efa70e4fabd1f6034411c9d4990';  // APIkey
+    final url = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+  //use openweathermap API to get future weather
+  Future<Map<String, dynamic>> fetchHourlyWeather(double latitude, double longitude) async {
+    final apiKey = '69516efa70e4fabd1f6034411c9d4990';  // API key
+    final url = 'https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$apiKey';
+    
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load weather forecast data');
+    }
+  }
+
+  //list future weather
+  Widget _hourlyWeatherListView() {
+  return Container(
+    height: 180.0, 
+    child: ListView.builder(
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(), // To provide a more compact scrolling experience
+      itemCount: _hourlyWeather.length,
+      itemBuilder: (context, index) {
+        var hourlyData = _hourlyWeather[index];
+        var time = DateTime.fromMillisecondsSinceEpoch(hourlyData['dt'] * 1000);
+        return ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 4.0), //ListTile inside margin
+          leading: Icon(Icons.wb_sunny, size: 20), 
+          title: Text(
+            '${time.hour}:00',
+            style: TextStyle(fontSize: 14), 
+          ),
+          subtitle: Text(
+            hourlyData['weather'][0]['description'],
+            style: TextStyle(fontSize: 12), 
+          ),
+          trailing: Text(
+            '${_convertToCelsius(hourlyData['main']['temp']).toStringAsFixed(1)} °C',
+            style: TextStyle(fontSize: 14), 
+          ),
+        );
+      },
+    ),
+  );
+}
+
+//page
+  Widget _assistantPage() {
+  return SingleChildScrollView(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 4.0,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Weather Information',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    _weatherInfo,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: getWeather,
+          child: Text('Get Weather'),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, 
+            backgroundColor: Color.fromARGB(255, 51, 126, 111),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          child: _hourlyWeatherListView(),
+        ),
+        _chatCard(),
+      ],
+    ),
+  );
+}
+
+Widget _chatCard() {
+  return Card(
+    margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+    elevation: 4.0,
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: messageController,
+            decoration: InputDecoration(
+              labelText: "Ask a question",
+              suffixIcon: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: sendMessage,
+              ),
+            ),
+            onSubmitted: (value) => sendMessage(),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Chat with OpenAI:',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 300,
+            child: ListView.builder(
+              itemCount: messages.length,
+              reverse: true,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(messages[index]));
+              },
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
 }
